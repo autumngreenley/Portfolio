df0aa2b7a66add9a46a0a3e640568dcb490e1cdb
autumngreenley <pokemonut@comcast.net>
2018-10-22 18:57:51 -0700

    Finishing up by adding in the conversions

--- HW4/HW4/Controllers/HomeController.cs
+++ HW4/HW4/Controllers/HomeController.cs
@@ -14,13 +14,39 @@ namespace HW4.Controllers
             return View();
         }
 
+        //POST
         public ActionResult ColorChooser()
         {
             return View();
         }
 
+        ///GET
+        [HttpGet]
         public ActionResult MileToMetric()
         {
+            double conversion = 0.0;
+            string givenunit = Request.QueryString["unit"];
+            double givenmiles = Convert.ToDouble(Request.QueryString["miles"]);
+            if(givenunit!=null && givenmiles!=null)
+            { 
+                if(givenunit=="mm")
+                {
+                    conversion = givenmiles * 1609340;
+                }
+                if(givenunit=="cm")
+                {
+                    conversion = givenmiles * 160934;
+                }
+                if(givenunit=="m")
+                {
+                    conversion = givenmiles * 1609.34;
+                }
+                if(givenunit=="km")
+                {
+                    conversion = givenmiles * 1.60934;
+                }
+                ViewBag.message = givenmiles + " miles is equal to " + conversion + " " + givenunit;
+            }
             return View();
         }
     }
