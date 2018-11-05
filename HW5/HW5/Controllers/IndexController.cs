983c95856704f1377647aa1af59c692f162084ae
autumngreenley <pokemonut@comcast.net>
2018-11-05 13:03:06 -0800

    Got the form submitting to the database correctly

--- HW5/HW5/Controllers/IndexController.cs
+++ HW5/HW5/Controllers/IndexController.cs
@@ -18,15 +18,50 @@ namespace HW5.Controllers
         {
             return View();
         }
-
-        public ActionResult RequestForm()
-        {
-            return View();
-        }
         
         public ActionResult DisplayList()
         {
             return View(database.Tennants.ToList());
+        }
+
+        public ActionResult RequestForm(string FirstName, string LastName, string Phone, string Apartment, int? UnitNum, string Explain, DateTime? Timestamps)
+        {
+            
+            if (FirstName == null || LastName == null || Phone == null || Apartment == null || Explain == null )
+            {
+                ViewBag.Blank = true;
+                ViewBag.Test = FirstName;
+                ViewBag.Test1 = LastName;
+                ViewBag.Test2 = Phone;
+                ViewBag.Test3 = Apartment;
+                ViewBag.Test4 = UnitNum;
+                ViewBag.Test5 = Explain;
+                ViewBag.Test6 = Timestamps;
+            }
+            else
+            {
+                ViewBag.Blank = false;
+            }
+
+            if (ViewBag.Blank == false)
+            {
+                Tennant NewTennant = new Tennant();
+                ViewBag.Test = "Hi!";
+                NewTennant.FirstName = FirstName;
+                NewTennant.LastName = LastName;
+                NewTennant.Phone = Phone;
+                NewTennant.Apartment = Apartment;
+                NewTennant.UnitNum = UnitNum ?? default(int); ;
+                NewTennant.Explain = Explain;
+                NewTennant.Timestamps = Timestamps ?? default(DateTime); ;
+                NewTennant.Timestamps = DateTime.Now;
+
+                ViewBag.NewTennant = NewTennant;
+                database.Tennants.Add(NewTennant);
+                database.SaveChanges();
+            }
+
+            return View();
         }
     }
 }
