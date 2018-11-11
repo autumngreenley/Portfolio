a6a4f781aec0c6f80fee5a9ca008ab44e35fda85
autumngreenley <pokemonut@comcast.net>
2018-11-10 19:50:46 -0800

    The detail page is now fully functional!

--- HW6/HW6/Controllers/HomeController.cs
+++ HW6/HW6/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Net;
+using HW6.Models.ViewModels;
 using HW6.Models;
 
 namespace HW6.Controllers
@@ -19,6 +20,24 @@ namespace HW6.Controllers
             ViewBag.message = query;
             //return View();
             return View(db.People.Where(p => p.FullName.ToLower().Contains(query.ToLower())).ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            Person user = db.People.Find(id);
+            PersonViewModel vm = new PersonViewModel
+            {
+                FullName = user.FullName,
+                Name = user.PreferredName,
+                Phone = user.PhoneNumber,
+                Fax = user.FaxNumber,
+                Email = user.EmailAddress,
+                MemberSince = user.ValidFrom
+            };
+
+            
+
+            return View(vm);
         }
     }
 }
--- HW6/HW6/Models/ViewModels/PersonViewModel.cs
+++ HW6/HW6/Models/ViewModels/PersonViewModel.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using HW6.Models;
-
 
 namespace HW6.Models.ViewModels
 {
-    public class PeopleViewModel
+    public class PersonViewModel
     {
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+
+        [DisplayName("Preferred Name")]
         public string Name { get; set; }
+
+        [DisplayName("Phone")]
+        public string Phone { get; set; }
+
+        [DisplayName("Fax")]
+        public string Fax { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Member Since")]
+        public System.DateTime MemberSince { get; set; }
     }
 }
