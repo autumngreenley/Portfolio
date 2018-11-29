83b219f2c84ec6e85539a79e415eb868953fad1a
autumngreenley <pokemonut@comcast.net>
2018-11-29 06:41:33 -0800

    Started the create form for Itmes

---
+++ HW8/HW8/HW8/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW8.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
