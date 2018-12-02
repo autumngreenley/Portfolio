7b45e2d7d874955aa1cc45850774488c17a6c9be
autumngreenley <pokemonut@comcast.net>
2018-12-02 15:23:48 -0800

    Parse the users text into an array (or two), ready to start calling to the API

--- HW7/HW7/Scripts/JavaScript.js
+++ HW7/HW7/Scripts/JavaScript.js
@@ -1,11 +1,30 @@
 ﻿$(document).ready(function () {
+    word = new Array("A");
+    allwords = new Array("A");
+    i = 0;
+    j = 0;
+    k = 0;
+
     $(".usertyping").keyup(function () {
-        last = $(".echo").html();
-        if (event.which == 32) {
-            $(".echo").html("Space!");
+        if (event.which == 32) { //If space is pressed...
+            allwords[i] = word.join("");
+            i++;
+            k = 0;
+            word = [];
+            $(".echo").html(allwords.join(" "));
+            j++;
         }
-        else {
-            $(".echo").html($("#texting").val());
+        //If the user presses something that isn't a char
+        else if ((event.which > 46 && event.which < 91) || event.which > 145) { //Otherwise, just print it in the div
+            word[k] = $("#texting").val().charAt(j);
+            k++;
+            j++;
+        }
+        //Aaaaand if the user presses backspace (like a moron, while testing)
+        else if (event.which == 8) {
+            j--;
+            k--;
+            word[k] = "";
         }
     });
 });
